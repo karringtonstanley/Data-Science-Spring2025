8405c1a64ea79bbb257f37b367ed0eb5e175464a
karringtonstanley <143848747+karringtonstanley@users.noreply.github.com>
2025-01-17 12:03:58 -0500

    Add files via upload

---
+++ example.r
@@ -0,0 +1,26 @@
+R version 4.4.2 (2024-10-31) -- "Pile of Leaves"
+Copyright (C) 2024 The R Foundation for Statistical Computing
+Platform: aarch64-apple-darwin20
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[R.app GUI 1.81 (8462) aarch64-apple-darwin20]
+
+[Workspace restored from /Users/karringtonstanley/.RData]
+[History restored from /Users/karringtonstanley/.Rapp.history]
+
+> (400+200)-50
+[1] 550
+> 
