cb4982c668f24dd2f8103009319ee9128b49579c
karringtonstanley <143848747+karringtonstanley@users.noreply.github.com>
2025-01-31 12:37:10 -0500

    Add files via upload

--- correlation-test.r
+++ correlation-test.r
@@ -13,3 +13,12 @@ my_data <- mtcars
 # Hypthesis: When wt of car is more, then mpg is less and when wt is less then mpg is more
 
 res <- cor.test(my_data$wt, my_data$mpg, method="pearson")
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
