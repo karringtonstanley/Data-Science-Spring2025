24a05c11ad493e0e490e27c2176532e4fd3dace3
karringtonstanley <143848747+karringtonstanley@users.noreply.github.com>
2025-02-24 12:40:41 -0500

    Add files via upload

---
+++ anova-test.r
@@ -0,0 +1,10 @@
+#Author: Karrington, Date: 02/24/25 Purpose: Test Anova Function
+
+# Importing data
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+#Create a hypothesis
+#Treatment types are effective against all poison types/there is a signifigant difference between treatment and poison types
+
+#Read the dataset in a variable
+df <- read.csv(PATH) 
